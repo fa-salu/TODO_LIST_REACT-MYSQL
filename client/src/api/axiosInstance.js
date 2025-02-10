e584157af3d7f792b5a7cb9360f4de9fbb6d2b82
fa-salu <fasalurahman7515@gmail.com>
2025-02-10 16:40:44 +0530

    url changed

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://todo-list-react-mysql.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
