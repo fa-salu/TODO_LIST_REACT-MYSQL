314c9ff4a0e2dfcd7ea9db270622bff0d69f1aad
fa-salu <fasalurahman7515@gmail.com>
2025-02-10 16:43:52 +0530

    api updated

--- client/src/api/FolderApi.js
+++ client/src/api/FolderApi.js
@@ -1,19 +1,17 @@
 import axiosInstance from "./axiosInstance";
 
-const API_URL = "http://localhost:5000/api";
-
 export const fetchFolders = async () => {
-  const response = await axiosInstance.get(`${API_URL}/folders`);
+  const response = await axiosInstance.get("/folders");
   return response.data;
 };
 
 export const addFolder = async (folderName) => {
-  const response = await axiosInstance.post(`${API_URL}/folders`, {
+  const response = await axiosInstance.post("/folders", {
     folderName,
   });
   return response.data;
 };
 
 export const deleteFolder = async (folderId) => {
-  await axiosInstance.delete(`${API_URL}/folders/${folderId}`);
+  await axiosInstance.delete(`/folders/${folderId}`);
 };
